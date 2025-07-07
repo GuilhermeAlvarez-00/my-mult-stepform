6e329e8e68b218d16481e4f5a7fcbc7b00b2e5c8
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2025-07-06 23:00:32 -0300

    feat(components|StepItem): creating component, tests and storybook

---
+++ src/components/StepItem/index.tsx
@@ -0,0 +1,38 @@
+import type { ReactNode } from "react"
+
+export interface IStepItem {
+	stepKey: ReactNode;
+	indicator: string;
+	label: string;
+	active: boolean;
+	short?: boolean;
+}
+
+export const StepItem = ({ stepKey, indicator, label, active, short }: IStepItem) => {
+	return (
+		<button
+			aria-label="step-item"
+			className="flex gap-4 items-center"
+		>
+			<div
+				aria-label="step-circle"
+				className={
+					` border border-white w-9 h-9 sm:w-10 sm:h-10 overflow-hidden rounded-full flex items-center justify-center 
+						${active ?
+						"bg-blue-200 text-black" :
+						"text-white"
+					}`
+				}
+			>
+				{stepKey}
+			</div>
+
+			{!short && (
+				<div aria-label="step-indicator" className="flex flex-col text-left text-white">
+					<span className="text-xs text-blue-300">{indicator}</span>
+					<strong>{label}</strong>
+				</div>
+			)}
+		</button>
+	)
+}
---
+++ src/components/StepItem/story/StepItem.stories.tsx
@@ -0,0 +1,57 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { FaReact } from "react-icons/fa";
+
+import { StepItem } from "../";
+
+type TMeta = Meta<typeof StepItem>;
+type TStory = StoryObj<typeof StepItem>;
+
+const meta: TMeta = {
+	title: "Components/StepItem",
+	component: StepItem,
+	tags: ["autodocs"],
+	parameters: {
+		backgrounds: {
+			default: "Purple"
+		}
+	}
+}
+
+export const Primary: TStory = {
+	args: {
+		stepKey: <span>1</span>,
+		indicator: "STEP 1",
+		label: "YOUR INFO",
+		active: false
+	},
+};
+
+export const Active: TStory = {
+	args: {
+		stepKey: <span>1</span>,
+		indicator: "STEP 1",
+		label: "YOUR INFO",
+		active: true
+	},
+};
+
+export const Short: TStory = {
+	args: {
+		stepKey: <span>1</span>,
+		indicator: "STEP 1",
+		label: "YOUR INFO",
+		active: false,
+		short: true
+	},
+};
+
+export const WithIcon: TStory = {
+	args: {
+		stepKey: <FaReact size={25} />,
+		indicator: "STEP 1",
+		label: "YOUR INFO",
+		active: false,
+	}
+};
+
+export default meta;
