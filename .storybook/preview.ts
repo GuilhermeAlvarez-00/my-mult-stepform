c6b165db660df7a3ef826473593c85d43cd79ffa
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2025-07-06 23:03:26 -0300

    refactor(storybook): setting default background color options

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,14 +2,16 @@ import type { Preview } from '@storybook/react'
 import "../src/styles.css"
 
 const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
-      },
-    },
-  },
+	parameters: {
+		backgrounds: {
+			values: [
+				{ name: 'Dark', value: '#333' },
+				{ name: 'White', value: '#fff' },
+				{ name: "Purple", value: "#493eff" },
+			],
+			default: "White"
+		},
+	},
 };
 
 export default preview;
