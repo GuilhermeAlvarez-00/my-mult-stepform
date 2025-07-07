ad12ab38b0e8f70d55244a17671fdc7fd041eb27
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2025-07-06 23:03:57 -0300

    feat(pages|MultiStepForm): main page to display form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { MultiStepForm } from "./pages/MultiStepForm"
+
 function App() {
 	return (
-		<div className="font-ubuntu">
-			<p>Hello world</p>
+		<div className="w-full min-h-[100dvh] bg-[#F0F0F0] grid place-items-center">
+			<MultiStepForm />
 		</div>
 	)
 }
