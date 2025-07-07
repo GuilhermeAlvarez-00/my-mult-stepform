97275e863db43863039eccd58ee05baddb7b3499
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2025-07-06 23:02:07 -0300

    feat(components|Step): component to show all steps

---
+++ src/components/Steps/index.tsx
@@ -0,0 +1,41 @@
+import { useScreen } from "../../hooks/useScreen";
+import { StepItem, type IStepItem } from "../StepItem";
+
+interface ISteps {
+  steps: IStepItem[];
+}
+
+const bgMobile = "/src/assets/images/bg-sidebar-mobile.svg";
+const bgDesktop = "/src/assets/images/bg-sidebar-desktop.svg";
+
+export const Steps = ({ steps }: ISteps) => {
+  const { width, breakpoints } = useScreen();
+
+  return (
+    <aside
+      style={{
+        backgroundImage: `url(${width >= breakpoints.md ? bgDesktop : bgMobile})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+      className={`relative w-full h-full md:rounded-lg`}
+    >
+      <div className="flex justify-center">
+        <ol className="flex gap-4 justify-center mt-8 md:flex-col md:gap-6">
+          {steps?.map((item) => (
+            <li key={item?.indicator}>
+              <StepItem
+                label={item?.label}
+                active={item?.active}
+                stepKey={item?.stepKey}
+                indicator={item?.indicator}
+                short={width <= breakpoints.md}
+              />
+            </li>
+          ))}
+        </ol>
+      </div>
+    </aside>
+  );
+};
