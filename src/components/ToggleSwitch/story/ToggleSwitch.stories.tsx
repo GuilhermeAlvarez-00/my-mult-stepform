26528d90356a6b8da159514b4ccdd3c1d93e5812
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2025-07-08 15:31:46 -0300

    feat(components|ToggleSwitch): creating component, tests and storybook

---
+++ src/components/ToggleSwitch/story/ToggleSwitch.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { ToggleSwitch } from "../";
+
+type TMeta = Meta<typeof ToggleSwitch>;
+type TStory = StoryObj<typeof ToggleSwitch>;
+
+const meta: TMeta = {
+  title: "Components/ToggleSwitch",
+  component: ToggleSwitch,
+  tags: ["autodocs"],
+};
+
+export const Primary: TStory = {
+  args: {
+    onChange: () => {},
+  },
+};
+
+export const PersistBg: TStory = {
+  args: {
+    persistBg: true,
+
+    onChange: () => {},
+  },
+};
+
+export const CustomBackgroundColor: TStory = {
+  args: {
+    onChange: () => {},
+    className: "bg-red-500",
+  },
+};
+
+export default meta;
